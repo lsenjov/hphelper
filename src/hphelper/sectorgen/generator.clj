ad138ecb29114c5ca6459502b2cbfa2fb9d8553d
lsenjov <logansenjov@gmail.com>
2016-02-16 09:21:28 +1000

    A bit more name generation and printing for sectors

--- src/hphelper/sectorgen/generator.clj
+++ src/hphelper/sectorgen/generator.clj
@@ -1,5 +1,6 @@
 (ns hphelper.sectorgen.generator
   (:require [hphelper.shared.sql :as sql]
+            [hiccup.core :refer :all]
             ))
 
 (defn create-service-group
@@ -14,4 +15,28 @@
                     (map (comp create-service-group :sg_id)
                          (sql/query "SELECT * FROM sg;")))
    :hps (reduce conj [] (repeatedly 6 (partial sql/get-random-name "U")))
+   :zone (sql/create-random-zone-name)
    })
+
+(defn print-sg-head
+  "Prints the sg head in a nice format"
+  [[k v :as sgh]]
+  (str (sql/get-sg-by-id k) ": " v))
+
+(defn html-print-sector
+  "Prints a single sector in a nice format"
+  [sec]
+  (html [:div
+         [:b "Zone:" (sec :zone) [:br]]
+         [:b "High Programmers:"] [:br]
+         (interpose [:br] (sort (sec :hps)))
+         [:br]
+         [:b "SG Heads"] [:br]
+         (interpose [:br] (sort (map print-sg-head (sec :sgheads))))
+         ]))
+
+(defn html-print-neighbours
+  "Prints a number of neighbouring randomly generated neighbours"
+  [n]
+  (html (map html-print-sector
+             (repeatedly n create-sector))))
