e2c8db11ab190638205d746c7b414e26367d01aa
lsenjov <logansenjov@gmail.com>
2016-06-01 17:18:02 +1000

    Changed number hiding to a better system

--- src/hphelper/shared/encrypt.clj
+++ src/hphelper/shared/encrypt.clj
@@ -4,18 +4,21 @@
     )
   )
 
-(def key-prime 7919)
+(def keyNum (+ 2000 (rand-int 5000)))
+(def offset (+ 10 (rand-int (- keyNum 20))))
 
 (defn int-hide
   "Hides an integer with the prime"
   [i]
   (if (string? i)
     (int-hide (Long/parseLong i))
-    (+ (rand-int key-prime) (* key-prime i))))
+    (+ offset (* keyNum i))))
 
 (defn int-show
-  "Unhides an integer with the prime"
+  "Unhides an integer with the prime, returns nil if invalid"
   [i]
   (if (string? i)
     (int-show (Long/parseLong i))
-    (long (/ i key-prime))))
+    (if (= offset (mod i keyNum))
+      (long (Math/floor (/ i keyNum)))
+      nil)))
